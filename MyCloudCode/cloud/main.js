98902caf95b7dabb91fb4ded8827938bdcf5dd7d
Andrew <ghobrial.andrew@gmail.com>
2014-03-24 23:28:54 -0400

    placeholder text for ebay parsing

--- MyCloudCode/cloud/main.js
+++ MyCloudCode/cloud/main.js
@@ -18,15 +18,15 @@ Parse.Cloud.define("eBayCategorySearch", function(request, response) {
              'SERVICE-VERSION' : '1.12.0', 
              'RESPONSE-DATA-FORMAT' : 'JSON', 
              'callback' : '_cb_findItemsByKeywords',
-             'itemFilter(3).name=ListingType' : 'itemFilter(3).value=FixedPrice',
+             'itemFilter(1).name=ListingType' : 'itemFilter(1).value=FixedPrice',
              'keywords' : 'request.params.item',
 
               // your other params
            },
-            success: function (httpResponse) {
-                console.log('eBay successfully pinged bro!');
-            // deal with success and respond to query
-},
+            success: function(results) {
+      //this is where you tell the cloud what to do with the data (AKA store the values it finds into parameters or into an array or whatever)
+      //response.success(whatever you want to print when it's successful);,
+    }
             error: function (httpResponse) {
                 console.log('error!!!');
                 console.error('Request failed with response code ' + httpResponse.status);
